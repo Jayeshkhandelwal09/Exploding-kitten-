0d907a5ec6b4d34cf610d766a5d4c243a565945f
Jayeshkhandelwal <khandelwaljayesh10@gmail.com>
2024-03-16 17:00:50 +0530

    Backend Completed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    const conn = await mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
     console.log(`MongoDB Connected Successfully`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
